5a51590403fdacc1d2dc97c51bf3eebb7c8f28c2
shantanubobade <shantanubobade513@gmail.com>
2026-05-27 23:50:51 +0530

    initial commit

---
+++ src/main/java/com/springboot/schema/ProductSchema.java
@@ -0,0 +1,31 @@
+package com.springboot.schema;
+
+import lombok.Builder;
+import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import java.math.BigDecimal;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class ProductSchema {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String category;
+    private String title;
+    private String description;
+    private BigDecimal price;
+    private String image;
+    private String rating;
+
+}
