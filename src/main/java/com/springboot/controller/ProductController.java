068b91ee7f71a22c011b4e121494b5fac855fc9f
shantanubobade <shantanubobade513@gmail.com>
2026-09-16 22:20:14 +0530

    Remove database dependencies for API deployment

--- src/main/java/com/springboot/controller/ProductController.java
+++ src/main/java/com/springboot/controller/ProductController.java
@@ -9,40 +9,40 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
-import com.springboot.schema.Product;
-import com.springboot.service.ProductService;
+//import com.springboot.schema.Product;
+//import com.springboot.service.ProductService;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/products")
 public class ProductController {
-    private final ProductService productService;
+//    private final ProductService productService;
+//
+//    @GetMapping
+//    public List<Product> getAllProducts() {
+//        return productService.findAll();
+//    }
+//
+//    @GetMapping("/{id}")
+//    public Product getProductById(@PathVariable Long id) {
+//        return productService.findById(id);
+//    }
+//
+//    @PostMapping
+//    public Product createProduct(@RequestBody Product product) {
+//        return productService.save(product);
+//    }
+//
+//    @PutMapping("/{id}")
+//    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+//        return productService.save(product);
+//    }
 
-    @GetMapping
-    public List<Product> getAllProducts() {
-        return productService.findAll();
-    }   
-
-    @GetMapping("/{id}")
-    public Product getProductById(@PathVariable Long id) {
-        return productService.findById(id);
-    }
-
-    @PostMapping
-    public Product createProduct(@RequestBody Product product) {
-        return productService.save(product);
-    }
-
-    @PutMapping("/{id}")
-    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
-        return productService.save(product);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteProductById(@PathVariable Long id) {
-        productService.deleteById(id);
-    }
+//    @DeleteMapping("/{id}")
+//    public void deleteProductById(@PathVariable Long id) {
+//        productService.deleteById(id);
+//    }
 
     @GetMapping("/returnName")
     public String updateProduct() {
